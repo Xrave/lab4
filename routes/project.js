5feaa597531194b7f728433680d013a90280241b
Xiaonan Tong <xiaonan@stanford.edu>
2014-01-29 15:06:06 -0800

    node.js lab complete

---
+++ routes/project.js
@@ -0,0 +1,14 @@
+
+/*
+ * GET home page.
+ */
+
+exports.viewProject = function(req, res){
+   //controller code.
+   var name = req.params.name;
+   console.log("The Project name is: " + name);
+   res.render('project', {
+    'projectName': name
+   });
+
+};
